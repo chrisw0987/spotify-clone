9ed9e915616bf4c4283a9531e5699a49ed267a09
chrisw0987 <wuc0987@gmail.com>
2025-06-12 15:37:16 -0400

    Initial Commit, Frontend Completed

---
+++ src/assets/assets.js
@@ -0,0 +1,186 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from  './song4.mp3'
+import song5 from  './song5.mp3'
+import song6 from  './song6.mp3'
+import song7 from  './song7.mp3'
+import song8 from  './song8.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:'The world’s most streamed hits, all in one chart',
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "K-Pop Wave",
+        image: img9,
+        desc:"Catch the hottest tracks riding the K-pop explosion",
+        bgColor:"#1b1643"
+    },
+    {   
+        id:2,
+        name: "Tokyo Beats",
+        image: img10,
+        desc:"Modern vibes meets tradition in this stylish J-pop blend",
+        bgColor:"#f4f0ea"
+    },
+    {   
+        id:3,
+        name: "TikTok Trending",
+        image: img16,
+        desc:"The tracks dominating your FYP — updated weekly",
+        bgColor:"#1a1a2e"
+    },
+    {   
+        id:4,
+        name: "Mega Hits",
+        image: img11,
+        desc:"Massive hits by global superstars — made to replay",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Feel-good tunes to brighten your day and lift your mood",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "On & On",
+        image: img1,
+        file:song1,
+        desc:"Cartoon, Jeja, Daniel Levi",
+        duration:"3:28"
+    },
+    {
+        id:1,
+        name: "HERO 〜希望の歌〜",
+        image: img2,
+        file:song2,
+        desc:"FLOW",
+        duration:"3:35"
+    },
+    {
+        id:2,
+        name: "Heroes Tonight",
+        image: img3,
+        file:song3,
+        desc:"Janji, Johnning",
+        duration:"3:28"
+    },
+    {
+        id:3,
+        name: "YES or YES",
+        image: img4,
+        file:song4,
+        desc:"TWICE",
+        duration:"3:57"
+    },
+    {
+        id:4,
+        name: "Cupid - Twin Version",
+        image: img5,
+        file:song5,
+        desc:"FIFTY FIFTY",
+        duration:"2:54"
+    },
+    {
+        id:5,
+        name: "California Gurls",
+        image: img6,
+        file:song6,
+        desc:"Katy Perry, Snoop Dog",
+        duration:"3:56"
+    },
+    {
+        id:6,
+        name: "Domino",
+        image: img7,
+        file:song7,
+        desc:"Jessie J",
+        duration:"3:54"
+    },
+    {
+        id:7,
+        name: "Night Dancer",
+        image: img12,
+        file:song8,
+        desc:"Imase",
+        duration:"3:31"
+    }
+]
